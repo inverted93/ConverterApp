8901ac47563718ae21a0ba80bff8d472cc2c64a6
inverted93 <mr.hoolm@gmail.com>
2016-09-15 16:48:55 +0200

    del

---
+++ Converter/app/src/main/java/hemmafix/converter/MainActivity.java
@@ -0,0 +1,89 @@
+package hemmafix.converter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Scanner;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void pressButton(View view){
+
+        EditText txtField = (EditText) findViewById(R.id.editText);
+        TextView info = (TextView) findViewById(R.id.textView);
+
+        String text = txtField.getText().toString();
+
+        Scanner sc = new Scanner(text);
+
+
+        String first = sc.next();
+
+        if(sc.hasNext()){
+
+            String second = sc.next();
+
+        }
+
+        double firstDouble = Double.parseDouble(first);
+
+        double res = pound(firstDouble);
+
+        info.setText("Kg " + res);
+
+        radioSet();
+
+
+
+    }
+
+
+
+    public double pound(double vikt){
+
+        double res = vikt * 0.45359237;
+
+
+        return res;
+
+
+    }
+
+
+    public void radioSet(){
+
+        RadioButton r1 = (RadioButton) findViewById(R.id.radioButton3);
+        RadioButton r2 = (RadioButton) findViewById(R.id.radioButton4);
+
+        RadioGroup radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
+
+
+        int selectedId = radioGroup.getCheckedRadioButtonId();
+
+        RadioButton selectedButton = (RadioButton) findViewById(selectedId);
+
+        Toast.makeText(MainActivity.this,selectedButton.getText(), Toast.LENGTH_SHORT).show();
+
+
+
+
+    }
+
+
+
+
+}
